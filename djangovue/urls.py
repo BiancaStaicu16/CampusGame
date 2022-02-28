329f70920fc9ff379ff8802cc267a1ab47c15e44
BHAVNA <bhavnasetty@gmail.com>
2022-02-28 20:31:38 +0000

    Created cards app

--- djangovue/urls.py
+++ djangovue/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from djangovue import views
+from djangovue import views, settings
+from django.conf.urls.static import static
 
 vue_urls = [
     path("", views.frontend),
@@ -25,4 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("", include(vue_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
