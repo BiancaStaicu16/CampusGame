70a0c62e5e907ff926ebc711f7290717916995a1
joshahk <joshahsaji@hotmail.co.uk>
2022-03-02 00:17:20 +0000

    removed the unnecessary code line

--- api/cards/models.py
+++ api/cards/models.py
@@ -5,6 +5,5 @@ class Card(TimestampedModel):
     title = models.CharField(max_length=255, unique=True)
     value = models.DecimalField(max_digits=19, decimal_places=2)
     picture = models.FileField(null=True, blank=True)
-    code = models.CharField(max_length=255, unique=True,default="000")
     def __str__(self):
         return "Card {}".format(self.title)
