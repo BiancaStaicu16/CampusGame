5ac048a458765b28196077e6e07e02d8db64ef9c
Maya Shah <89914643+mgosrani02@users.noreply.github.com>
2022-03-23 12:04:58 +0000

    Update ScanQR.vue and added some styling

--- frontend/src/views/ScanQR.vue
+++ frontend/src/views/ScanQR.vue
@@ -1,62 +1,76 @@
-<template>
-    <div id="app">
-                <div class="card">
-                    <qrcode-stream width@decode="onDecode" @init="onInit"/>
-                    <p> class="center">THIS IS JUST A TEST</p>
-                </div>
+<template id="scanqr">
+  <div
+    id="app"
+    style="
+      width: 74%;
+      height: 75%;
+      margin-left: auto;
+      margin-right: 1%;
+      background-color: white;
+      border-radius: 25px;
+    "
+  >
+    <h1 style="color: black">Scan QR Code Page</h1>
+
+    <div
+      class="card"
+      style="
+        width: 50%;
+        height: 50%;
+        margin-left: auto;
+        margin-right: auto;
+        padding-bottom: 5%;
+      "
+    >
+      <qrcode-stream @decode="onDecode" @init="onInit" />
     </div>
+  </div>
 </template>
 
 <script>
-import { QrcodeStream } from 'vue-qrcode-reader'
+import { QrcodeStream } from "vue-qrcode-reader";
 export default {
-  name: 'app',  
-
+  name: "app",
+  data() {},
   components: { QrcodeStream },
   methods: {
-    async onDecode (result) {
-      var audio = new Audio(require('../assets/unlock.mp3'))
-      audio.play()
-      window.location.replace(result)
+    async onDecode(result) {
+      window.location.replace(result);
     },
-    async onInit (promise) {
+    async onInit(promise) {
       try {
-        await promise
+        await promise;
       } catch (error) {
-        if (error.name === 'NotAllowedError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: you need to grant camera access permisson"
-        } else if (error.name === 'NotFoundError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: no camera on this device"
-        } else if (error.name === 'NotSupportedError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: secure context required (HTTPS, localhost)"
-        } else if (error.name === 'NotReadableError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: is the camera already in use?"
-        } else if (error.name === 'OverconstrainedError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: installed cameras are not suitable"
-        } else if (error.name === 'StreamApiNotSupportedError') {
-          this.alert = 'alert-danger'
-          this.status = "ERROR: Stream API is not supported in this browser"
+        if (error.name === "NotAllowedError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: you need to grant camera access permisson";
+        } else if (error.name === "NotFoundError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: no camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.alert = "alert-danger";
+          this.status = "ERROR: Stream API is not supported in this browser";
         }
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
-<style>
-#app {
-  color: #404040;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-size:30px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center+50px;
-  color: #FFFFFFFF;
-  width: 65vh;
-  margin-top: 2vh;
-  margin-left: 20vh;
+<style scoped>
+h1 {
+  left: 0;
+  font-size: 2em;
+}
+
+p a {
+  color: white;
 }
